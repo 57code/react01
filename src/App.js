0e364fe6115e32e87727d3e66099ee246e1b2942
yt0379 <yt0379@qq.com>
2019-07-29 13:54:56 +0800

    ff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import JsxTest from "./components/JsxTest";
 import StateMgt from "./components/StateMgt";
 import EventHandle from "./components/EventHandle";
 import ContextTest from "./components/ContextTest";
-import HocTest from "./components/HocTest.tsx";
+import HocTest from "./components/HocTest.js";
 import Composition from "./components/Composition";
 import HooksTest from "./components/HooksTest";
 
